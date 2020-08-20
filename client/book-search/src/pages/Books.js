ef06269da1ac2e83648e927ea0fe6aa20f069f35
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-20 01:17:43 -0700

    books on my list return

--- client/book-search/src/pages/Books.js
+++ client/book-search/src/pages/Books.js
@@ -51,11 +51,34 @@ function Books() {
                     >
                         Submit Book
                     </FormBtn>
-
                 </form>
             </Col>
-        </Row>
-
+            <Col size="md-6 sm-12">
+              <Jumbotron>
+                  <h1>Books on My List</h1>
+              </Jumbotron>
+              {books.length ? (
+                  <List>
+                    {books.map(book => {
+                        return (
+                            <ListItem key={books._id}>
+                          <a href={"/books/" + books._id}>
+                            <strong>
+                              {books.title} by {books.author}
+                            </strong>
+                          </a>
+                          <DeleteBtn onClick={() => {}} />
+                      </ListItem>
+                        );
+                    })}
+                    </List>
+              ) : (
+                  <h3> No Results to Display</h3>
+              )}
+            </Col>
+         </Row>
       </Container>
-    )
+    );
 }
+
+export default Books;
