75ed951ee3d53b169d4662f5e5576ce2a385bd0d
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-25 00:59:12 -0700

    api routes for crud app

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Jumbotron from "../components/Jumbotron";
 import DeleteBtn from "../components/DeleteBtn";
 // import API from "../utils/API";
@@ -6,77 +6,103 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, FormBtn } from "../components/Form";
 import axios from "axios";
-require('dotenv').config();
+import API from "../utils/API";
+require("dotenv").config();
 
- function Books() {
+function Books() {
   // react access the api key in the .env
   const apiKey = process.env.REACT_APP_API_KEY;
-  
+
   const [book, setBook] = useState("");
   const [result, setResult] = useState([]);
-  
+  //************************************ */
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  function loadBooks() {
+    API.getBooks()
+      .then((res) => setBooks(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  function deleteBook(id) {
+    API.deleteBook(id)
+      .then((res) => loadBooks())
+      .catch((err) => console.log(err));
+  }
+
+  //************************************ */
   function handleChange(event) {
     const book = event.target.value;
     setBook(book);
   }
+ //***************************************/
+ function saveBook() {
+   
+ }
+  //***************************************/
   
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(book);
-  
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey)
-    .then(data => {
-      console.log(data.data.items);
-      setResult(data.data.items);
-    })
-  }
-    return (
-      <Container fluid>
-        <Row>
-            <Col size="md-6">
-                <Jumbotron>
-                    <h1>Google Book Search</h1>
-                </Jumbotron>
-                <form>
-                    <Input
-                      onChange={handleChange}
-                      onSubmit={handleSubmit}
-                      name="title"
-                      placeholder="Title (required)"
-                    />
-                    <FormBtn>
-                        Submit Book
-                    </FormBtn>
-                </form>
-                              {result.length ? (
-                  <List>
-                    {result.map(book => {
-                      console.log(JSON.stringify(book, null, 2))
-                        return (
-                            <ListItem key={book._id}>
-                          <a href={"/books/" + book._id}>
-                            <strong>
-                              {book.volumeInfo.title} by {book.volumeInfo.authors}
-                            </strong>
-                          </a>
-                          <DeleteBtn onClick={() => {}} />
-                      </ListItem>
-                        );
-                    })}
-                    </List>
-              ) : (
-                  <h3> No Results to Display</h3>
-              )}
 
-            </Col>
-            {/* <Col size="md-6 sm-12">
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          book +
+          "&key=" +
+          apiKey
+      )
+      .then((data) => {
+        console.log(data.data.items);
+        setResult(data.data.items);
+      });
+  }
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6">
+          <Jumbotron>
+            <h1>Google Book Search</h1>
+          </Jumbotron>
+          <form>
+            <Input
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <FormBtn>Submit Book</FormBtn>
+          </form>
+          {result.length ? (
+            <List>
+              {result.map((book) => {
+                console.log(JSON.stringify(book, null, 2));
+                return (
+                  <ListItem key={book._id}>
+                    <a href={"/books/" + book._id}>
+                      <strong>
+                        {book.volumeInfo.title} by {book.volumeInfo.authors}
+                      </strong>
+                    </a>
+                    <DeleteBtn onClick={() => {}} />
+                  </ListItem>
+                );
+              })}
+            </List>
+          ) : (
+            <h3> No Results to Display</h3>
+          )}
+        </Col>
+        {/* <Col size="md-6 sm-12">
               <Jumbotron>
                   <h1>Books on My List</h1>
               </Jumbotron>
             </Col> */}
-         </Row>
-      </Container>
-    );
+      </Row>
+    </Container>
+  );
 }
-
 export default Books;
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 // // import React, { useState } from "react";
 import Jumbotron from "../components/Jumbotron";
 // import DeleteBtn from "../components/DeleteBtn";
-// // import API from "../utils/API";
+import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 // import { List, ListItem } from "../components/List";
 // import { Input, FormBtn } from "../components/Form";
 
 
 function SavedBooks() {
+  // const [book, setBook] = useState({})
+
+  // const {id} = useParams()
+  // useEffect(() => {
+  //   API.getBook(id)
+  //    .then(res => setBook(res.data))
+  //    .catch(err => console.log(err));
+  //  }, []) 
+  
+
     return(
         <Container fluid>
           <Row>
